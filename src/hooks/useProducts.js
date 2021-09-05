a7cc47950be40358bc3a3202a8a79ac997cc7d3a
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-09-05 23:16:30 +0530

    useEffect cleanup and skeleton ui implemented

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,23 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const useProducts = () => {
   const [products, setProducts] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+  const isMounted = useRef(false);
   const fetchProducts = async () => {
     try {
+      isMounted.current && setIsFetching(true);
       const result = await fetch("https://fakestoreapi.com/products");
       const data = await result.json();
-      setProducts(data);
+      isMounted.current && setProducts(data);
+      isMounted.current && setIsFetching(false);
     } catch (error) {
       console.log(error);
+      isMounted.current && setIsFetching(false);
     }
   };
   useEffect(() => {
+    isMounted.current = true;
     fetchProducts();
-    return () => {};
+    return () => (isMounted.current = false);
   }, []);
 
   return {
     products,
+    isFetching,
   };
 };
 
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,22 +1,61 @@
 import { useProducts } from "../hooks";
 import { ProductCard } from "../components";
-import { Breadcrumbs, Container, Grid, Typography } from "@material-ui/core";
+import {
+  Breadcrumbs,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
 const Shop = () => {
-  const { products } = useProducts();
+  const { products, isFetching } = useProducts();
   return (
-    <Container
-      style={{
-        marginTop: "2vh",
-      }}
-    >
-      <Breadcrumbs>
+    <Container>
+      <Breadcrumbs
+        style={{
+          margin: "2vh 0",
+        }}
+      >
         <Typography color="textPrimary">E-Commerce</Typography>
         <Typography>Products</Typography>
       </Breadcrumbs>
       <Grid container spacing={2}>
-        {products.map((product) => (
-          <ProductCard product={product} key={product._id} />
-        ))}
+        {isFetching
+          ? Array(12)
+              .fill("skeleton-item-")
+              .map((item, i) => (
+                <Grid item key={item + i} lg={3}>
+                  <Card>
+                    <Skeleton variant="rect" width={"100%"} height={150} />
+                    <CardContent>
+                      <Skeleton variant="rect" width={"100%"} height={10} />
+                      <Skeleton
+                        variant="rect"
+                        width={"100%"}
+                        height={10}
+                        style={{ marginTop: 10 }}
+                      />
+                      <Skeleton
+                        variant="rect"
+                        width={"100%"}
+                        height={15}
+                        style={{ marginTop: 15 }}
+                      />
+                      <Skeleton
+                        variant="rect"
+                        width={"100%"}
+                        height={35}
+                        style={{ marginTop: 15 }}
+                      />
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))
+          : products.map((product) => (
+              <ProductCard product={product} key={product.id} />
+            ))}
       </Grid>
     </Container>
   );
